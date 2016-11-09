897c6869d55d92dcd25c2fefbdc85fe5734e0b07
Wim Godden <wim@wimgodden.be>
2016-11-09 22:36:01 +0100

    Added PHP 7.0 and 7.1 tests Added PHPCompatibility

--- .travis.install.sh
+++ .travis.install.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
 elif [ "$TRAVIS_PHP_VERSION" = '5.3.3' ] ; then
     ## Can't update on php 5.3.3 as openssl is now required in the latest build and not available on php5.3.3 at travis
-    composer update --prefer-source --no-dev -g disable-tls true
+    composer update --prefer-source --no-dev --disable-tls
     composer dump-autoload
 else
     composer self-update
